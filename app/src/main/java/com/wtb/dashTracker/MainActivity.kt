04fd1db3ec6d60fca5366a3e8e0484f874e72422
wesley <wtbenica@gmail.com>
2021-12-07 02:58:11 -0800

    fab menu has animations now. looks really good, just need to add the last two actions;

--- app/src/main/java/com/wtb/dashTracker/MainActivity.kt
+++ app/src/main/java/com/wtb/dashTracker/MainActivity.kt
@@ -1,19 +1,32 @@
 package com.wtb.dashTracker
 
+import android.animation.ArgbEvaluator
+import android.animation.ValueAnimator
+import android.content.Context
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.graphics.drawable.AnimatedVectorDrawable
 import android.os.Bundle
+import android.util.TypedValue
 import android.view.Gravity
 import android.view.View
 import android.view.View.GONE
 import android.view.View.VISIBLE
 import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import androidx.activity.viewModels
+import androidx.annotation.AttrRes
+import androidx.annotation.ColorInt
 import androidx.annotation.IdRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.fragment.app.FragmentManager
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.wtb.dashTracker.databinding.ActivityMainBinding
@@ -26,12 +39,31 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import java.time.DayOfWeek
 import java.time.LocalDate
 
+@ColorInt
+fun getColorAccent(context: Context) = getAttrColor(context, R.attr.colorAccent)
+
+@ColorInt
+fun getColorPrimary(context: Context) = getAttrColor(context, R.attr.colorPrimary)
+
+@ColorInt
+fun getAttrColor(context: Context, @AttrRes id: Int): Int {
+    val tv = TypedValue()
+    val arr = context.obtainStyledAttributes(tv.data, intArrayOf(id))
+    @ColorInt val color = arr.getColor(0, 0)
+    arr.recycle()
+    return color
+}
+
 @ExperimentalCoroutinesApi
 class MainActivity : AppCompatActivity(), DailyFragment.DailyFragmentCallback {
 
     private lateinit var binding: ActivityMainBinding
     private var fabMenuIsVisible = false
     private var fabMenuItems = mutableListOf<FabMenuButton>()
+    private val fabOpenAnimation: Animation
+        get() = AnimationUtils.loadAnimation(this, R.anim.open_fab_menu)
+    private val fabCloseAnimation: Animation
+        get() = AnimationUtils.loadAnimation(this, R.anim.close_fab_menu)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -63,8 +95,7 @@ class MainActivity : AppCompatActivity(), DailyFragment.DailyFragmentCallback {
                 if (fabMenuIsVisible)
                     hideFabMenu()
                 else
-                    makeFabMenu()
-                fabMenuIsVisible = !fabMenuIsVisible
+                    showFabMenu()
             }
         }
 
@@ -77,53 +108,112 @@ class MainActivity : AppCompatActivity(), DailyFragment.DailyFragmentCallback {
         viewModel.thisWeek.observe(this) {
             binding.actMainThisWeek.text = getString(R.string.currency_unit, it)
         }
+
+        initFabMenu()
     }
 
     private fun hideFabMenu() {
-        for (item in fabMenuItems) {
-            item.visibility = GONE
+        fabMenuItems.forEachIndexed { index, item ->
+            item.animate()
+                .alpha(0f)
+                .setStartDelay(ANIM_DELAY * (fabMenuItems.size - index))
+                .setDuration(ANIM_LENGTH)
+                .withStartAction { item.visibility = VISIBLE }
         }
+
+        @ColorInt val accent = getColorAccent(this)
+        @ColorInt val gray = Color.parseColor("#333344")
+        ValueAnimator.ofObject(ArgbEvaluator(), gray, accent).apply {
+            duration = ANIM_LENGTH
+            addUpdateListener {
+                val i = animatedValue as Int
+                binding.fab.backgroundTintList = ColorStateList.valueOf(i)
+            }
+            start()
+        }
+        binding.fab.setImageResource(R.drawable.close_fab)
+        when (val d = binding.fab.drawable) {
+            is AnimatedVectorDrawableCompat -> d.start()
+            is AnimatedVectorDrawable -> d.start()
+        }
+
+        fabMenuIsVisible = false
     }
 
-    private fun makeFabMenu() {
+    private fun showFabMenu() {
+        fabMenuItems.forEachIndexed { index, item ->
+            item.animate()
+                .alpha(1f)
+                .setStartDelay(ANIM_DELAY * index)
+                .setDuration(ANIM_LENGTH)
+                .withStartAction { item.visibility = VISIBLE }
+        }
 
-        if (fabMenuItems.isEmpty()) {
-            val menuItems: List<FabMenuButtonInfo> = listOf(
-                FabMenuButtonInfo(
-                    "Add Entry",
-                    R.drawable.calendar,
-                    { DetailFragment().show(supportFragmentManager, "new_entry_dialog") }
-                )
-            )
+        @ColorInt val accent = getColorAccent(this)
+        @ColorInt val gray = Color.parseColor("#333344")
+        val anim = ValueAnimator.ofObject(ArgbEvaluator(), accent, gray)
+        anim.duration = ANIM_LENGTH
+        anim.addUpdateListener {
+            val i = anim.animatedValue as Int
+            binding.fab.backgroundTintList = ColorStateList.valueOf(i)
+        }
+        anim.start()
 
-            @IdRes var itemAnchor: Int = binding.fab.id
-            for (item in menuItems) {
-                val newMenuItem: FabMenuButton =
-                    FabMenuButton.newInstance(this, item, binding.root).apply {
-                        elevation = R.dimen.fab_menu_spacing.toFloat()
-                        id = View.generateViewId()
-                    }
-                fabMenuItems.add(newMenuItem)
-                binding.root.addView(
-                    newMenuItem,
-                    CoordinatorLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT).apply {
-                        anchorId = itemAnchor
-                        anchorGravity = Gravity.TOP or Gravity.END
-                        gravity = Gravity.TOP
-                    })
+        binding.fab.setImageResource(R.drawable.avd_anim)
+        when (val d = binding.fab.drawable) {
+            is AnimatedVectorDrawableCompat -> d.start()
+            is AnimatedVectorDrawable -> d.start()
+        }
 
-                itemAnchor = newMenuItem.id
-            }
-        } else {
-            for (fabMenuItem in fabMenuItems) {
-                fabMenuItem.visibility = VISIBLE
-            }
+        fabMenuIsVisible = true
+    }
+
+    private fun initFabMenu() {
+        @IdRes var itemAnchor: Int = binding.fab.id
+        for (item in getMenuItems(supportFragmentManager)) {
+            val newMenuItem: FabMenuButton =
+                FabMenuButton.newInstance(this, item, binding.root).apply {
+                    elevation = R.dimen.fab_menu_spacing.toFloat()
+                    id = View.generateViewId()
+                }
+            fabMenuItems.add(newMenuItem)
+            binding.root.addView(
+                newMenuItem,
+                CoordinatorLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT).apply {
+                    anchorId = itemAnchor
+                    anchorGravity = Gravity.TOP or Gravity.END
+                    gravity = Gravity.TOP
+                })
+
+            itemAnchor = newMenuItem.id
+        }
+        for (item in fabMenuItems) {
+            item.visibility = GONE
         }
     }
 
     companion object {
         const val APP = "GT_"
         const val TAG = APP + "MainActivity"
+        const val ANIM_LENGTH = 300L
+        const val ANIM_DELAY = 25L
+
+        private fun getMenuItems(fm: FragmentManager): List<FabMenuButtonInfo> = listOf(
+            FabMenuButtonInfo(
+                "Add Entry",
+                R.drawable.calendar
+            ) { DetailFragment().show(fm, "new_entry_dialog") },
+            FabMenuButtonInfo(
+                "Add Adjustment",
+                R.drawable.alert,
+                { }
+            ),
+            FabMenuButtonInfo(
+                "Add Payout",
+                R.drawable.chart,
+                { }
+            )
+        )
 
         fun getThisWeeksDateRange(): Pair<LocalDate, LocalDate> {
             val todayIs = LocalDate.now().dayOfWeek
